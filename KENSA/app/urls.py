3bf312f23d6d5c25c288deea32e6399df053c640
GabrielNarvadez <131564153+GabrielNarvadez@users.noreply.github.com>
2025-06-22 22:43:36 +0800

    Starter Kit uploaded

--- KENSA/app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from app import views 
-from django.contrib import admin  
-
-urlpatterns = [
-    path('', views.index, name='index'),  # Main index view
-    path('admin/', admin.site.urls),  # Django Admin
-]
